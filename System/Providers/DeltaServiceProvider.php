35836bcf004f9b85497cc6e31ab19117074eb0cc
Ramon Linares <rlinareslf@gmail.com>
2022-12-01 14:20:58 -0400

    Proyecto Dealer

---
+++ System/Providers/DeltaServiceProvider.php
@@ -0,0 +1,22 @@
+<?php namespace Delta\Providers;
+
+/*
+ *---------------------------------------------------------
+ * ©IIPEC
+ * Santo Domingo República Dominicana.
+ *---------------------------------------------------------
+*/
+
+use Illuminate\Support\ServiceProvider;
+
+class DeltaServiceProvider extends ServiceProvider {
+	
+	public function boot() {
+	}
+
+	public function register() {
+	}
+}
+
+
+/* End of providers DeltaServiceProvider.php */
